b338ed1415872cca377adfeb07e0ab032abca1f0
oooyu429 <y_nnnn22@naver.com>
2019-01-22 18:18:45 +0900

    테스트

--- artgo/src/main/java/com/bitcamp/artgo/board/model/BoardDto.java
+++ artgo/src/main/java/com/bitcamp/artgo/board/model/BoardDto.java
@@ -16,4 +16,59 @@ public class BoardDto {
   private int status;
   private int mno;
   private int bcode;
+public int getBno() {
+	return bno;
+}
+public void setBno(int bno) {
+	this.bno = bno;
+}
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getContent() {
+	return content;
+}
+public void setContent(String content) {
+	this.content = content;
+}
+public int getHit() {
+	return hit;
+}
+public void setHit(int hit) {
+	this.hit = hit;
+}
+public int getDate() {
+	return date;
+}
+public void setDate(int date) {
+	this.date = date;
+}
+public int getStatus() {
+	return status;
+}
+public void setStatus(int status) {
+	this.status = status;
+}
+public int getMno() {
+	return mno;
+}
+public void setMno(int mno) {
+	this.mno = mno;
+}
+public int getBcode() {
+	return bcode;
+}
+public void setBcode(int bcode) {
+	this.bcode = bcode;
+}
+@Override
+public String toString() {
+	return "BoardDto [bno=" + bno + ", title=" + title + ", content=" + content + ", hit=" + hit + ", date=" + date
+			+ ", status=" + status + ", mno=" + mno + ", bcode=" + bcode + "]";
+}
+  
+  
 }
